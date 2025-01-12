904c28d38c53b4ccb153e9a552913bd00f35187d
Andrey <andrey404demyanovsky@gmail.com>
2025-01-12 15:33:36 +0300

    Добавлен функция read_line в модуль управления терминалом

--- terminal_management.py
+++ terminal_management.py
@@ -1,5 +1,9 @@
+import asyncio
 import sys
 import shutil
+from asyncio import StreamReader
+from typing import Deque
+from collections import deque
 
 
 def delete_line():
@@ -31,3 +35,21 @@ def move_to_down_of_screen() -> int:
     total_rows -= 1
     sys.stdout.write(f"\033[{total_rows}E")
     return total_rows
+
+
+async def read_line(stdin_reade: StreamReader):
+    DELETE_SIGN = b"\x7f"
+    buffer: Deque = deque()
+
+    while (char := await stdin_reade.read(1)) != b"":
+
+        if char == DELETE_SIGN:
+            if len(buffer) > 0:
+                move_back_one_char()
+                sys.stdout.write(" ")
+                move_back_one_char()
+                buffer.pop()
+        else:
+            buffer.append(char)
+            sys.stdout.write(char.decode())
+            sys.stdout.flush()
